9d296eca1c71ded85a45f91b3015748444ae3858
Mangata1993 <49599921+Mangata1993@users.noreply.github.com>
2021-08-01 18:10:22 -0700

    Create 407. Trapping Rain Water II.py

---
+++ leetcode_practice/407. Trapping Rain Water II.py
@@ -0,0 +1,25 @@
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        if not heightMap or not heightMap[0]:
+            return 0
+        m, n = len(heightMap), len(heightMap[0])
+        visited = [[0] * n for _ in range(m)]
+        heap = []
+        res = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or i == m - 1 or j == 0 or j == n - 1:
+                    heapq.heappush(heap, (heightMap[i][j], i, j))
+                    visited[i][j] = 1
+        
+        while heap:
+            height, i, j = heapq.heappop(heap)
+            for x, y in ((i-1, j), (i+1, j), (i, j-1), (i, j+1)):
+                if 0 <= x < m and 0 <= y < n and not visited[x][y]:
+                    res += max(0, height - heightMap[x][y])
+                    heapq.heappush(heap, (max(heightMap[x][y], height), x, y))
+                    visited[x][y] = 1
+        
+        return res
+                    
